4f0337d030b6619958943279f74ac80c18202506
Michael Valenta <mfvalenta@gmail.com>
2021-11-10 14:41:02 -0500

    Fun stuff

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        String createAnotherCharacter;
+        do {
+            System.out.println("Welcome to the D & D character generator!");
+
+            System.out.println("What is the name of your character?");
+            String name = input.readLine();
+            if (name.trim().length() == 0) {
+                break;
+            }
+
+            Character c = new Character(name);
+            c.generateAttributes();
+            System.out.println("Here is your character");
+            System.out.println(c.toString());
+
+            System.out.println("Would you like to generate a character?");
+            createAnotherCharacter = input.readLine();
+
+        } while (createAnotherCharacter.equalsIgnoreCase("yes"));
     }
 }
---
+++ src/Character.java
@@ -0,0 +1,79 @@
+/**
+ * This class represents a D & D character
+ */
+public class Character {
+    private String name;
+    private int strength, dexterity, intelligence, wisdom, constitution, charisma;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public void generateAttributes() {
+        this.strength = generateAttribute();
+        this.dexterity = generateAttribute();
+        this.intelligence = generateAttribute();
+        this.wisdom = generateAttribute();
+        this.constitution = generateAttribute();
+        this.charisma = generateAttribute();
+    }
+
+    private int generateAttribute() {
+        return 18;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Name: ");
+        buffer.append(this.name);
+        buffer.append('\n');
+        buffer.append("Strength: ");
+        buffer.append(this.strength);
+        buffer.append('\n');
+        buffer.append("Dexterity: ");
+        buffer.append(this.dexterity);
+        buffer.append('\n');
+        buffer.append("Intelligence: ");
+        buffer.append(this.intelligence);
+        buffer.append('\n');
+        buffer.append("Wisdom: ");
+        buffer.append(this.wisdom);
+        buffer.append('\n');
+        buffer.append("Constitution: ");
+        buffer.append(this.constitution);
+        buffer.append('\n');
+        buffer.append("Charisma: ");
+        buffer.append(this.charisma);
+        buffer.append('\n');
+        return buffer.toString();
+    }
+}
